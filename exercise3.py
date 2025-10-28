bc4069b8bed6c33521c42f4176b7912da527552a
zara <93386493+zezl7@users.noreply.github.com>
2025-10-28 21:32:56 +0100

    Fix duplication by defining an interval function + Fix TypeError through add method in exercise 2 (#4)
    Co-authored-by: Zara Zlatanova <zara.zlatanova@sap.com>

--- exercise3.py
+++ exercise3.py
@@ -32,8 +32,8 @@ class Rectangle:
         #         and reuse that here.
         ll_px = point.x - self._lower_left.x
         ll_py = point.y - self._lower_left.y
-        return ll_px >= 0 and ll_px <= self._dx \
-            and ll_py >= 0 and ll_py <= self._dy
+        return self.is_in_interval(ll_px, 0, self._dx) \
+            and self.is_in_interval(ll_py, 0, self._dy)
 
     def _is_idx_on_upper_edge(self, i: int) -> bool:
         return i in [2, 3]
@@ -41,7 +41,8 @@ class Rectangle:
     def _is_idx_on_right_edge(self, i: int) -> bool:
         return i in [1, 3]
 
-    # def is_in_interval(...) -> bool: # Task A
+    def is_in_interval(self, value: float, min_val: float, max_val: float) -> bool: # Task A
+        return value >= min_val and value <= max_val
 
 
 def test_rectangle_contains_exact() -> None:
